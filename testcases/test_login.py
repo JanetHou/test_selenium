a0a290f71b4a8d1537047f16a45e12530e711818
Jia <316441091@qq.com>
2022-11-01 23:29:16 +0800

    修改path

--- testcases/test_login.py
+++ testcases/test_login.py
@@ -17,7 +17,7 @@ class TestLogin:
         options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
         options.add_argument('--disable-dev-shm-usage')  # 客服资源有限的问题
         # self.driver = webdriver.Chrome("../chromedriver.exe")
-        self.driver = webdriver.Chrome(chrome_options=options)
+        self.driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=options)
         self.driver.get(c_data.home_url)
         self.driver.maximize_window()
 
