89aefed556a8e8ef1b6ea7b06bd900f561e0b349
Jia <316441091@qq.com>
2022-11-02 22:05:50 +0800

    修改变量

--- testcases/test_login.py
+++ testcases/test_login.py
@@ -12,12 +12,13 @@ import sys
 
 class TestLogin:
     def setup(self):
-        options = Options()
-        options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
-        options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
-        options.add_argument('--disable-dev-shm-usage')  # 客服资源有限的问题
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+        chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
+        chrome_options.add_argument('--disable-dev-shm-usage')  # 客服资源有限的问题
         # self.driver = webdriver.Chrome("../chromedriver.exe")
-        self.driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=options)
+        self.driver=webdriver.Chrome()
+        self.driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=chrome_options)
         self.driver.get(c_data.home_url)
         self.driver.maximize_window()
 
